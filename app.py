279b11bf55dcb11e9c2dc71d9b9e5fc030c68820
RizwanHaq11 <134547524+RizwanHaq11@users.noreply.github.com>
2025-04-02 15:24:36 +0530

    added requirements

--- app.py
+++ app.py
@@ -155,4 +155,5 @@ def check_ip():
 
 if __name__ == '__main__':
     logger.info("Starting Flask application")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
